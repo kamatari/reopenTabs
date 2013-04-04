1f1b9bd1d6a643ca53bd88e76349cf803f7c6919
Masahiro Kamata <8.incidents.sr.9@gmail.com>
2013-04-05 08:54:20 +0900

    add tabs.oncreated

--- reopenTabs.js
+++ reopenTabs.js
@@ -2,23 +2,40 @@ chrome.browserAction.setBadgeText({text:"100"});
 
 var microsecondsPerDay = 1000 * 60 * 60 * 24;
 var dayBefore = (new Date).getTime() - microsecondsPerDay;
+var closeTabArray = new Array();
 
 chrome.tabs.onRemoved.addListener(
 	function(tabId89) {
 		chrome.tabs.get(tabId89, function(tab){
-				alert(tab)
-			})
-//		chrome.history.search({
-//			'text': '',
-//			'startTime':dayBefore
-//			},
-//			function(history){
-//				var closedTabs = history.shift()
-//				alert(closedTabs.url);
-//			}
-//		);
+
+		})
 	}
 )
+
+//
+chrome.history.search({
+	'text': '',
+	'startTime':dayBefore
+	},
+	function(history){
+		//console.log(history);
+		//var closedTabs = history.shift()
+		//alert(closedTabs.url);
+	}
+);
+
+chrome.tabs.onCreated.addListener(
+	function(openTab) {
+		console.log(openTab)
+	}
+)
+
+chrome.history.onVisited.addListener(
+	function(new_history) {
+		closeTabArray.push(new_history.url);
+		//alert(closeTabArray);
+	}
+);
 
 chrome.browserAction.onClicked.addListener(function() {
 	//var action_url = 'http://google.com/';
