4c6710a8b786328ecad85f8a100d911ef09a4a96
Masahiro Kamata <8.incidents.sr.9@gmail.com>
2013-04-06 07:26:35 +0900

    extension runs normally

--- reopenTabs.js
+++ reopenTabs.js
@@ -1,34 +1,31 @@
-chrome.browserAction.setBadgeBackgroundColor({color:[116, 169, 214, 255]})
+var openedTabArray	= new Array();
+var closedTabArray	= new Array();
+var badgeColor		= new Array(116, 169, 214, 255);
+var stockTabsCount	= 0;
 
-var microsecondsPerDay = 1000 * 60 * 60 * 24;
-var dayBefore = (new Date).getTime() - microsecondsPerDay;
-var openTabArray = new Array();
-var closeTabArray = new Array();
-var closedTabsCount;
-
-chrome.history.search({
-	'text': '',
-	'startTime':dayBefore
-	},
-	function(history){
-		//console.log(history);
-		//var closedTabs = history.shift()
-		//alert(closedTabs.url);
-	}
-);
+chrome.browserAction.setBadgeBackgroundColor({color:badgeColor})
 
 chrome.tabs.onCreated.addListener(
 	function(openTab) {
-		openTabArray[openTab.id] = openTab.url;
+		openedTabArray[openTab.id] = openTab.url;
 	}
 )
 
 chrome.tabs.onRemoved.addListener(
-	function(closeTabId) {
-		closeTabArray.push(openTabArray[closeTabId]);
-		closedTabsCount = closeTabArray.length;
-		chrome.browserAction.setBadgeText({text:String(closedTabsCount)});
+	function(closedTabId) {
+		closedTabArray.push(openedTabArray[closedTabId]);
+		updateBadgeCount();
 	}
 )
 
-//chrome.browserAction.onClicked.addListener(function(tabs.Tab tab) {...});
+chrome.browserAction.onClicked.addListener(
+	function() {
+		chrome.tabs.create({"url":closedTabArray.pop(),"selected":false});
+		updateBadgeCount();
+	}
+);
+
+function updateBadgeCount() {
+	stockTabsCount = closedTabArray.length;
+	chrome.browserAction.setBadgeText({text:String(stockTabsCount)});
+}
